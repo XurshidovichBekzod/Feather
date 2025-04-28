e0e6e9a7f29041468eb6cc22536b61cba1e80ee8
XurshidovichBekzod <xurshidovichbekzod@gmail.com>
2025-04-28 21:10:28 +0500

    Feather

---
+++ js/javascript.js
@@ -0,0 +1,81 @@
+// 1
+// {
+//     function kopaytma(a){
+//         return a * 2
+//     }
+//     console.log(kopaytma(10))
+// }
+
+
+//2 
+// {
+//     function ism(fname){
+//         return `Salom ${fname}`
+//     }
+//     console.log(ism('Bekzod'))
+// }
+
+
+//3
+// {
+//     function musbat(a){
+//         if(a > 0){
+//             return `${a} musbat son`
+//         }else if(a < 0){
+//             return `${a} manfiy son`
+//         }else{
+//             return `${a} ga teng`
+//         }
+//     }
+
+//     console.log(musbat(10));
+//     console.log(musbat(-10));
+//     console.log(musbat(0));   
+// }
+
+
+//4
+// {
+//     function juft(a){
+//         if(a % 2){
+//             return `${a} juft son` 
+//         }else{
+//             return `${a} toq son`
+//         }
+//     }
+//     console.log(juft(10))
+//     console.log(juft(7))
+// }
+
+
+//5
+// {
+//     function kattasi(a , b){
+//         return a > b ? a : b 
+//     }
+//     console.log(kattasi(10 , 11))
+// }
+
+
+//6
+// {
+//     function birplus(a) {
+//         for (let i = 1; i <= a; i++)
+//             console.log(i);
+//         return birplus 
+//     }
+//     birplus(10)
+// }
+
+
+//8
+// {
+//     function teskariSoz(soz) {
+//         let yangiSoz = '';
+//         for (let i = soz.length - 1; i >= 0; i--) {
+//           yangiSoz += soz[i];
+//         }
+//         return yangiSoz;
+//       }
+//       console.log(teskariSoz("assalomu alaykum")); // "molas"
+// }
